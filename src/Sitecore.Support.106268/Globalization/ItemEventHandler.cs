e9931f07025cbacccd747e1d156b9c1c81ba69e0
DK\KIV <kirill.volovitskiy@sitecore.net>
2018-10-23 18:07:55 +0300

    handle version removed

--- src/Sitecore.Support.106268/Globalization/ItemEventHandler.cs
+++ src/Sitecore.Support.106268/Globalization/ItemEventHandler.cs
@@ -10,6 +10,7 @@ using Sitecore.Diagnostics;
 using Sitecore.Events;
 using Sitecore.Globalization;
 using System.Reflection;
+using Sitecore.Data.Events;
 
 namespace Sitecore.Support.Globalization
 {
@@ -18,15 +19,20 @@ namespace Sitecore.Support.Globalization
         private void OnItemSavedRemote(object sender, EventArgs args)
         {
             Assert.ArgumentNotNull(sender, "sender");
-            Assert.ArgumentNotNull((object)args, "args");
-            using (new LanguageFallbackItemSwitcher(new bool?(true)))
+            Assert.ArgumentNotNull(args, "args");
+
+            ItemSavedRemoteEventArgs itemSavedRemoteEventArgs = args as ItemSavedRemoteEventArgs;
+            if (itemSavedRemoteEventArgs != null && itemSavedRemoteEventArgs.Item != null && !(itemSavedRemoteEventArgs.Item.TemplateID != TemplateIDs.DictionaryEntry))
             {
-                Sitecore.Globalization.ItemEventHandler itemEventHandler = new Sitecore.Globalization.ItemEventHandler();
-                string str = (string)itemEventHandler.GetType().InvokeMember("OnItemSavedRemote", BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.InvokeMethod, (Binder)null, (object)itemEventHandler, new object[2]
+                using (new LanguageFallbackItemSwitcher(new bool?(IsItemFallbackEnabled(itemSavedRemoteEventArgs.Item))))
                 {
-          sender,
-          (object) args
-                });
+                    Sitecore.Globalization.ItemEventHandler itemEventHandler = new Sitecore.Globalization.ItemEventHandler();
+                    string str = (string)itemEventHandler.GetType().InvokeMember("OnItemSavedRemote", BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.InvokeMethod, (Binder)null, (object)itemEventHandler, new object[2]
+                    {
+                        sender,
+                        (object) args
+                    });
+                }
             }
         }
 
@@ -37,7 +43,8 @@ namespace Sitecore.Support.Globalization
             Item parameter = Event.ExtractParameter(args, 0) as Item;
             if (parameter == null)
                 return;
-            using (new LanguageFallbackItemSwitcher(new bool?(true)))
+            //using (new LanguageFallbackItemSwitcher(new bool?(true)))
+            using (new LanguageFallbackItemSwitcher(new bool?(IsItemFallbackEnabled(parameter))))
             {
                 if (parameter.TemplateID == TemplateIDs.DictionaryEntry)
                     this.UpdateCacheEntries(parameter[FieldIDs.DictionaryKey], parameter[FieldIDs.DictionaryKey], parameter);
@@ -49,6 +56,69 @@ namespace Sitecore.Support.Globalization
                 });
             }
         }
+
+        private void OnVersionRemovedRemote(object sender, EventArgs args)
+        {
+            Assert.ArgumentNotNull(sender, "sender");
+            Assert.ArgumentNotNull(args, "args");
+
+            VersionRemovedRemoteEventArgs itemSavedRemoteEventArgs = args as VersionRemovedRemoteEventArgs;
+            if (itemSavedRemoteEventArgs != null && itemSavedRemoteEventArgs.Item != null && !(itemSavedRemoteEventArgs.Item.TemplateID != TemplateIDs.DictionaryEntry))
+            {
+                Sitecore.Globalization.ItemEventHandler itemEventHandler = new Sitecore.Globalization.ItemEventHandler();
+                string str = (string)itemEventHandler.GetType().InvokeMember("OnVersionRemovedRemote", BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.InvokeMethod, (Binder)null, (object)itemEventHandler, new object[2]
+                {
+                        sender,
+                        (object) args
+                });
+
+                var parameter = itemSavedRemoteEventArgs.Item;
+
+                if (parameter.TemplateID == TemplateIDs.DictionaryEntry && parameter.Versions.Count == 0 && IsItemFallbackEnabled(parameter))
+                {
+                    DictionaryDomain dictionaryDomainForEntry = this.FindDictionaryDomainForEntry(parameter, parameter.Parent);
+                    if (dictionaryDomainForEntry != null)
+                    {
+                        using (new LanguageFallbackItemSwitcher(new bool?(true)))
+                        {
+                            var fallbackItem = parameter.GetFallbackItem();
+                            if (fallbackItem != null)
+                                CachePhraseVersionRemoved(fallbackItem[FieldIDs.DictionaryKey], parameter, dictionaryDomainForEntry);
+
+                        }
+                    }
+                }
+            }         
+        }
+
+        private void OnVersionRemoved(object sender, EventArgs args)
+        {
+            Assert.ArgumentNotNull(sender, "sender");
+            Assert.ArgumentNotNull((object)args, "args");
+            Item parameter = Event.ExtractParameter(args, 0) as Item;
+            if (parameter == null)
+                return;
+
+            Sitecore.Globalization.ItemEventHandler itemEventHandler = new Sitecore.Globalization.ItemEventHandler();
+            string str = (string)itemEventHandler.GetType().InvokeMember("OnVersionRemoved", BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.InvokeMethod, (Binder)null, (object)itemEventHandler, new object[2]
+            {
+                sender,
+                (object) args
+            });
+
+            if (parameter.TemplateID == TemplateIDs.DictionaryEntry && parameter.Versions.Count == 0 && IsItemFallbackEnabled(parameter))
+            {
+                DictionaryDomain dictionaryDomainForEntry = this.FindDictionaryDomainForEntry(parameter, parameter.Parent);
+                if (dictionaryDomainForEntry != null)
+                {
+                    using (new LanguageFallbackItemSwitcher(new bool?(true)))
+                    {
+                        CachePhraseVersionRemoved(parameter[FieldIDs.DictionaryKey], parameter, dictionaryDomainForEntry);
+                    }
+                }
+            }
+        }
+        
 
         internal void UpdateCacheEntries(string oldDictionaryKey, string newDictionaryKey, Item item)
         {
@@ -110,6 +180,23 @@ namespace Sitecore.Support.Globalization
             }
         }
 
+        private void CachePhraseVersionRemoved(string dictionaryKey, Item item, DictionaryDomain domain)
+        {
+            Item obj = item.Database.GetItem(item.ID, item.Language, item.Version);
+            if (obj != null)
+            {
+                var fallbackItem = obj.GetFallbackItem();
+                if (fallbackItem != null)
+                {
+                    obj.Fields[FieldIDs.DictionaryPhrase].GetValue(true, true);
+                    Translate.CachePhrase(dictionaryKey, fallbackItem[FieldIDs.DictionaryPhrase], obj.Language, domain);
+
+                    Translate translate = new Translate();
+                    string str = (string)translate.GetType().InvokeMember("Save", BindingFlags.Instance | BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.InvokeMethod, (Binder)null, (object)translate, new object[0]);
+                }                               
+            }
+        }
+
         internal void UpdateCacheEntries(string oldDictionaryKey, string newDictionaryKey, Item item, DictionaryDomain domain)
         {
             if (domain == null)
@@ -120,6 +207,22 @@ namespace Sitecore.Support.Globalization
             item.RuntimeSettings.ForceModified = true;
             Translate translate = new Translate();
             string str = (string)translate.GetType().InvokeMember("Save", BindingFlags.Instance | BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.InvokeMethod, (Binder)null, (object)translate, new object[0]);
+        }
+
+        private bool IsItemFallbackEnabled(Item item)
+        {
+            if (item != null)
+            {
+                if (!StandardValuesManager.IsStandardValuesHolder(item))
+                {
+                    using (new LanguageFallbackItemSwitcher(false))
+                    {
+                        return item.Fields[FieldIDs.EnableItemFallback].GetValue(true, true, false) == "1";
+                    }
+                }
+                return item.Fields[FieldIDs.EnableItemFallback].GetValue(false) == "1";
+            }
+            return false;
         }
     }
 }
